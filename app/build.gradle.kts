e90a8c890ed2d940a44000f165a48ad7ecb78dce
XiAnzheng <kevin.cristopher01@gmail.com>
2024-06-05 00:36:33 +0700

    Update Compose Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,8 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata")
     // Optional - Integration with RxJava
     implementation("androidx.compose.runtime:runtime-rxjava2")
+    // compose navigation
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
 
 
     implementation("androidx.core:core-ktx:1.13.1")
